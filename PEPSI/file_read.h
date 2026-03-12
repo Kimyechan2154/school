fe66800539ab564b3069a2f40267b0e0051615f0
Kimyechan2154 <kimyechan2959@gmail.com>
2026-03-12 19:20:25 +0900

    Update and rename PEPSI/file_read.h to PEPSI/DPA/file_read.h

--- PEPSI/file_read.h
+++
@@ -1,7 +0,0 @@
-#ifndef FILE_READ//파일 구조와 역할 설명
-#define FILE_READ
-
-int ptFileRead(uint8_t** pt, FILE* ptFile);//평문 텍스트 파일 읽음
-int traceFileRead(float** trace, FILE* traceFile);//암호문 텍스트 파일 읽음
-
-#endif
